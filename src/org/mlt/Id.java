1514aa28df26ca25be6e7d8d088d5c0bf20d48c3
Luke Crest <bromstargames@gmail.com>
2019-10-25 20:30:23 +0200

    Reimplement Id Generator and add Id class
    Main Id Generator, implemented as MainIdGenerator, has been reprogrammed as singleton
    so that now it supports polymorphism.
    IdGenerator interface has been added.
    Id class has been added to construct a save wrapper which will make id modification impossible.
    Testing code has reimplemented to follow given interface.

---
+++ src/org/mlt/Id.java
@@ -0,0 +1,13 @@
+package org.mlt;
+
+public class Id {
+	
+	
+	public Id(IdGenerator gen)
+	{
+		
+		
+		
+	}
+	
+}
---
+++ src/tests/MainTest.java
@@ -0,0 +1,48 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+import org.mlt.IdGenerator;
+import org.mlt.Main;
+
+class MainTest extends Main{
+	
+	
+		@Test
+		public void testIdGenerator() throws Exception
+		{
+			 IdGenerator generator = Main.MainIdGenerator.getInstance();
+			 int x=generator.getId(); //0
+			 assertEquals(x, 0); 
+			 
+			 Integer y = generator.getId();
+			 assertEquals(y, new Integer(1));//1
+			 
+			 generator.getId(); //2
+			 generator.getId();//3
+			 generator.freeId(x);
+			 generator.freeId(y);
+			 generator.freeId(2);
+			
+			 y = generator.getId(); //2
+			 assertEquals(y, new Integer(2));
+			 
+			 generator.freeId(0);
+			 generator.freeId(x);
+			 
+			 y = generator.getId(); //1
+			 assertEquals(y, new Integer(1));
+			 
+			 assertThrows(Exception.class, () ->
+	         { 
+	        	 for(int i=0; i<=30; i++) generator.getId();
+	         });
+				
+		
+	}
+	
+	
+
+}
